eb4dee3262ad6ba1bbffa6067fdb4e85c3c8c6ec
Kalyanram <kalyanramr2007@gmail.com>
2026-06-18 21:47:18 +0530

    Move Firebase config to environment variables

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,44 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDDFrSfVgDO08CLImPHCmA-lY6CQXq9hac",
-  authDomain: "hospital-queue-managemen-93f2c.firebaseapp.com",
-  projectId: "hospital-queue-managemen-93f2c",
-  storageBucket: "hospital-queue-managemen-93f2c.firebasestorage.app",
-  messagingSenderId: "428298275828",
-  appId: "1:428298275828:web:491c0778c9fce41bebc7a1",
-  measurementId: "G-43R05HYQY7"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 export { app, analytics, db };
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDDFrSfVgDO08CLImPHCmA-lY6CQXq9hac",
-//   authDomain: "hospital-queue-managemen-93f2c.firebaseapp.com",
-//   projectId: "hospital-queue-managemen-93f2c",
-//   storageBucket: "hospital-queue-managemen-93f2c.firebasestorage.app",
-//   messagingSenderId: "428298275828",
-//   appId: "1:428298275828:web:491c0778c9fce41bebc7a1",
-//   measurementId: "G-43R05HYQY7"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
